93690f0b67a6cfba656814cf21c3af4d5e151739
itsgitz <anggit.ginanjar@outlook.com>
2021-03-25 08:39:32 +0700

    add: How to add color text in go.

---
+++ advanced/color/simple/go.mod
@@ -0,0 +1,5 @@
+module github.com/itsgitz/go-workshop/advanced/color/simple
+
+go 1.15
+
+require github.com/fatih/color v1.10.0 // indirect
---
+++ advanced/color/simple/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("Simple Color Workshop")
+
+	// simple color
+	color.Cyan("[*] I'm Cyan!")
+	color.Blue("[*] I'm Blue?")
+	color.Red("[*] Red is the dead of color")
+}
